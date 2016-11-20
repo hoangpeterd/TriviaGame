7591ac15fc3e57cf4fae563c67598716416f54a4
Peter Hoàng <peterhoang@utexas.edu>
2016-11-20 05:00:44 -0600

    timer added, but need to fix the if statement that loops the modal pop up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,20 +6,45 @@ var unanswered = 0;
 // end variables
 
 //hide and display divs
-$('#startButton').click(function() {
-	$('#startPage').hide();
-	$('#triviaPage').show();
+$("#startButton").click(function() {
+	$("#startPage").hide();
+	$("#triviaPage").show();
 });
 
-$('#submitButton').click(function() {
-	$('#triviaPage').hide();
-	$('#resultsPage').show();
+$("#submitButton").click(function() {
+	$("#triviaPage").hide();
+	$("#resultsPage").show();
 });
 //end hide and display divs
 
 //timer functions
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
 
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
 
+        display.text(minutes + ":" + seconds);
+
+        // modal pops up when time runs out
+        if (--timer < 0) {
+            timer = duration;
+            $("#timeUpModal").modal("show");
+            $("#modalButton").click(function() {
+            	$("#submitButton").click();
+            });
+        }
+    }, 1000);
+}
+
+jQuery(function ($) {
+    var twoMinutes = 10,
+        display = $("#timeRemaining");
+    startTimer(twoMinutes, display);
+});
 // end timer functions
 
 //function that checks the answers
